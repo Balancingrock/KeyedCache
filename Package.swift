ae3caed5f3a0ba93abd6cfe61d7b397dbfd878b6
Balancingrock <github@balancingrock.nl>
2017-04-02 09:19:24 +0200

    Updated BRUtils to 0.2.0

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "KeyedCache",
     dependencies: [
-        .Package(url: "https://github.com/Balancingrock/BRUtils", Version(0, 1, 0))
+        .Package(url: "https://github.com/Balancingrock/BRUtils", Version(0, 2, 0))
     ]
 )
