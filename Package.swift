e4728c2dd216d5637cddd8c8738b2bc1218a9780
Balancingrock <rien@balancingrock.nl>
2020-05-14 14:16:18 +0200

    switch dependency back to github

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "KeyedCache", targets: ["KeyedCache"])
     ],
     dependencies: [
-        .package(url: "../BRUtils", from: "1.1.0")
+        .package(url: "https://github.com/Balancingrock/BRUtils", from: "1.1.0")
     ],
     targets: [
         .target(
